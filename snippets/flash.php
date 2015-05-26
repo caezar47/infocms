3c872360fae1e46a148188aeca1c0ae73d6e67a4
caezar47 <iss47@ya.ru>
2015-05-26 08:13:02 +0300

    код для вставки flash баннера

---
+++ snippets/flash.php
@@ -0,0 +1,24 @@
+<div class="boxing banner">
+	<object type="application/x-shockwave-flash" data="/flash/240_400.swf" style="width: 240px; height: 400px;" >
+		<param name="movie" value="/flash/240_400.swf">
+		<param name="quality" value="high">
+		<param name="wmode" value="transparent">
+		<param name="swfversion" value="6.0.65.0">
+		<!-- Следующий тег object не поддерживается браузером Internet Explorer. Поэтому скройте его от Internet Explorer при помощи IECC. -->
+		<!--[if !IE]>-->
+		<object type="application/x-shockwave-flash" data="/flash/240_400.swf" style="width: 240px; height: 400px;">
+		<!--<![endif]-->
+			<param name="quality" value="high">
+			<param name="wmode" value="window">
+			<param name="scale" value="exactfit">
+			<param name="loop" value="true">
+			<param name="swfversion" value="6.0.65.0">
+			<param name="expressinstall" value="Scripts/expressInstall.swf">
+			<!-- Браузер отображает следующее альтернативное содержимое для пользователей Flash Player 6.0 и более старых версий. -->	
+			<!-- заглушка -->
+			<a href="specialnye-predlozhenija.php"><img src="/flash/lock.jpg" alt="" title="" /></a>			
+		<!--[if !IE]>-->
+		</object>
+		<!--<![endif]-->
+	</object>
+</div>
